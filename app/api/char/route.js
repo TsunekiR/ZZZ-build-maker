ec665a3d090449603e30d052dc54172c73f44e14
Roberto Tsuneki <tsunekiroberto@gmail.com>
2025-02-25 23:44:17 -0300

    commented post

--- app/api/char/route.js
+++ app/api/char/route.js
@@ -2,12 +2,12 @@ import char from "@/models/char";
 import connectMongoDB from "@/libs/mongodb";
 import { NextResponse } from "next/server";
 
-export async function POST(request) {
-    const {charName, charType, charBaseAttack} = await request.json();
-    await connectMongoDB();
-    await char.create({charName, charType, charBaseAttack});
-    return NextResponse.json({message: "Char created"}, {status: 201});
-}
+// export async function POST(request) {
+//     const {charName, charType, charBaseAttack} = await request.json();
+//     await connectMongoDB();
+//     await char.create({charName, charType, charBaseAttack});
+//     return NextResponse.json({message: "Char created"}, {status: 201});
+// }
 
 export async function GET() {
     await connectMongoDB();
