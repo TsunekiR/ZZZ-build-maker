7940fd3d894b8e2bf2bf0109b9a9f506cc71377b
André Martini Diniz <andrediniz1310@gmail.com>
2025-03-03 22:19:45 -0500

    - Changed CharSelect to have a filter, group characters by faction and an avatar image related to the character

--- components/CharSelect.jsx
+++ components/CharSelect.jsx
@@ -6,44 +6,95 @@ import {
     SelectTrigger,
     SelectValue,
   } from "@/components/ui/select"
+import { cn, getAvatarImg } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import React from "react";
+import { ChevronDown, ChevronsUpDown, Check } from "lucide-react"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  ShieldQuestion,
+} from "@/components/ui/popover"
+import { useMemo, useState } from "react";
+import Image from "next/image";
 
 export default function CharSelect(props) {
     // Props
     const propValue = props.value;
-    const propsValues = props.values;
+    const propValues = props.values;
     const propOnChange = props.onChange;
+    const [open, setOpen] = useState(false)
+
+    const charsByFaction = useMemo(() => {
+        if (!propValues) return null;
+        const charsByFaction = {};
+        propValues.forEach((char) => {
+            if (!charsByFaction[char.charInfo.charFaction]) {
+                charsByFaction[char.charInfo.charFaction] = [];
+            }
+            charsByFaction[char.charInfo.charFaction].push(char);
+        });
+        return charsByFaction;
+    }, [propValues])
 
     return (
         <>
         {/* TODO: Add Loading... */}
 
-        {propsValues && propsValues.length === 0 && <p>No characters found</p>}
+        {propValues && propValues.length === 0 && <p>No characters found</p>}
 
-        {propsValues && propsValues.length !== 0 &&
-            <div className="flex flex-row gap-4">
-                <Select onValueChange={propOnChange} value={propValue}>
-                    <SelectTrigger>
-                        <SelectValue placeholder="Please select a character" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        {propsValues?.map((char) => (
-                            <SelectItem key={char.charInfo.charName} value={char.charInfo.charName}>
-                                {char.charInfo.charName}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </Select>
+        {propValues && propValues.length !== 0 &&
+            <div className="flex flex-row gap-4 h-full">
+                <Popover open={open} onOpenChange={setOpen}>
+                    <PopoverTrigger asChild>
+                        <Button
+                            variant="outline"
+                            role="combobox"
+                            aria-expanded={open}
+                            className="font-normal w-[200px] justify-between"
+                            >
+                            {propValue? 
+                                propValue
+                                : "Select character..."}
+                            <ChevronsUpDown className="opacity-50" />
+                        </Button>
+                    </PopoverTrigger>
+                    <PopoverContent className="w-[200px] p-0">
+                        <Command>
+                            <CommandInput placeholder="Search character..." className="h-9" />
+                            <CommandList>
+                                <CommandEmpty>No character found.</CommandEmpty>
+                                {charsByFaction && Object.keys(charsByFaction).map((faction) => (
+                                    <CommandGroup key={faction} heading={faction}>
+                                        {charsByFaction[faction].map((char) => (
+                                            <CommandItem
+                                                key={char.charInfo.charName}
+                                                value={char.charInfo.charName}
+                                                onSelect={(currentValue) => {
+                                                    propOnChange(currentValue)
+                                                    setOpen(false)
+                                                    }}>
+                                                <Image alt={char.charInfo.charName} src={getAvatarImg(char.charInfo.charName)} width={20} height={20} />
+                                                {char.charInfo.charName}
+                                                <Check className={cn("ml-auto", propValue === char.charInfo.charName ? "opacity-100" : "opacity-0")}/>
+                                            </CommandItem>
+                                        ))}
+                                    </CommandGroup>
+                                ))}
+                            </CommandList>
+                        </Command>
+                    </PopoverContent>
+                </Popover>
                 
                 <Button variant="destructive" onClick={() => propOnChange("")}>Clear</Button>
-                {/* <select name="charSelect" id="charSelect" value={propValue} onChange={(e) => onChangeSelect(e.target.value)}>
-                    {propsValues?.map((char) => (
-                        <option key={char.charInfo.charName} value={char.charInfo.charName}>
-                        {char.charInfo.charName}
-                        </option>
-                        ))}
-                        </select> */}
             </div>
         }
         </>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'act-webstatic.hoyoverse.com',
+            port: '',
+            pathname: '/**',
+            search: '',
+          },
+        ],
+      },
+};
 
 export default nextConfig;
