d8b9d94fb512a33875f7691ea0d1e2e8fcbecc97
Roberto Tsuneki <tsunekiroberto@gmail.com>
2025-03-30 01:17:35 -0300

    Kinda fixed BuilderTable(again). Added slider component(unused)

--- components/builder/BuilderTable.jsx
+++ components/builder/BuilderTable.jsx
@@ -12,12 +12,28 @@ const renderRow = (label, value) => {
 const BuilderTable = (props) => {
     const { data, title, rowsPerLine } = props;
 
+    //TODO: FAZER ESTA MERDA FICAR DINAMICA
+    let wRow = ""
+    switch(rowsPerLine) {
+        case 1 :
+            break
+        case 2 :
+            wRow = "xl:w-1/2";
+            break;
+        case 3 :
+            wRow = "xl:w-1/3";
+            break;
+        default:
+            console.log("BuilderTable warning: rowsPerLine must be [1-3]")
+            break;
+    }
+
     return (
         <div className="p-2 flex flex-row flex-wrap xl:pr-0 w-full">
             <p className="text-3xl self-center">{title}</p>
             <div className='flex flex-wrap w-full'>
                 {data.map((row, index) => (
-                    <div style={{width: "calc(1/"+ rowsPerLine +" * 100%)"}} className={"flex justify-between flex-col xl:pr-2"} key={index}>
+                    <div className={"flex justify-between flex-col xl:pr-2 w-full " + wRow} key={index}>
                         {renderRow(row.label, row.value)}
                     </div>
                 ))}
